bbb676176575a32fc27a8c405ba403741f017001
VirtualMachine <VirtualMachine@VIRTUAL-MACHINE>
2013-05-01 17:04:58 -0500

    css malo

--- public/layouts/headerUser.php
+++ public/layouts/headerUser.php
@@ -1,18 +1,33 @@
 <html>
     <head>
-        <title> The Process Network </title>
         <link rel='stylesheet' href="../stylesheets/style.css">
-        <link rel='stylesheet' href="../stylesheets/login.css">
+        <link rel='stylesheet' href="../stylesheets/profile.css">
     </head>
     <body>
 
-        <header>
-            <div id="banner">
-                <h1>The Process Network</h1>
+        <nav id="mainMenu">
+            <div class="menu">
+                <a class="navIcon" href="index.html"><img title="Index" alt="Index" src="../images/home.png" width="22" height="22"/></a>
+                <div class="navVerticalDivision"></div>
                 <div class="navLeftItem">
                     <form method="get" action="/search" id="search">
                         <input name="q" type="text" size="40" placeholder="Buscar..." />
                     </form>
                 </div>
+
+                <div class="navRightItem">
+                    <a class="navIcon" href="#">perfil</a>
+
+                    <div class="navVerticalDivision"></div>
+
+                    <a class="navIcon" href="#">
+                        <img title="Configuracion" alt="Configuracion" src="../images/settings.png" width="22" height="22" /></a>
+
+                    <div class="navVerticalDivision"></div>
+
+                    <a class="navIcon" href="#"><img title="Salir" alt="Salir" src="../images/logout.png" width="22" height="22" /></a>
+
+                </div>
+
             </div>
-        </header>
+        </nav>
--- public/user/user.main.php
+++ public/user/user.main.php
@@ -5,13 +5,87 @@ if (!$session->is_logged_in()) {
 }
 ?>
 <?php include_layout_template('headerUser.php'); ?>
-<?php include_layout_template('navigationUser.php'); ?>
 
 <div id="wrapper">
     <section id="main">
 
-        <p>Bienvenido <?php echo User::find_by_id($session->user_id)->name; ?></p>
-    </section>
-</div>
+        <aside id="userInfo">
 
-<?php include_layout_template('footerUser.php'); ?>
+            <div id="avatar">
+
+                <h2>User Name</h2>				<!-- seek with php-->
+                <br/>
+                <span class="avatarImage" >
+                    <img src="image/user.png">
+                </span>
+                <br/><br/>
+            </div>
+
+
+            <div id="userOptions">
+
+                <h3>Num. Posts</h3>
+                <p>xxxxxxx</p>					<!-- seek with php-->
+                <h3>Fecha de Creaci&oacute;n</h3>
+                <p>xx-xx-xxxx</p>				<!-- seek with php-->
+                <br/>
+                <a href="#" >Ver Perfil</a><br/><br/>
+                <a href="#" >Editar Perfil</a>
+
+            </div>
+
+        </aside><!-- user info -->
+
+
+
+        <article class="posts">
+
+            <h2>Posts</h2>
+
+            <nav>
+                <div class="menu">
+
+                    <ul>
+
+                        <li><a href="#">Fecha</a>
+
+                            <ul>
+                                <li><a href="#">Up-Down</a></li>
+                                <li><a href="#">Down-Up</a></li>
+                            </ul>
+
+                        </li>
+
+                        <li><a href="#">Rating</a></li>
+
+                        <li><a href="#">Visitas</a></li>
+
+                    </ul>
+
+                </div>
+            </nav>
+
+
+            <div id="postList">
+
+
+
+            </div><!-- post list -->
+
+
+        </article><!-- posts -->
+
+
+
+        <aside id="createPost">
+            <div>
+                <input type="submit" value="Crear Post" />
+            </div>
+        </aside><!-- Crear Post -->
+
+
+
+        <section><!-- end of main -->
+            </div><!-- end of wrapper -->
+
+            <?php include_layout_template('footerUser.php'); ?>
